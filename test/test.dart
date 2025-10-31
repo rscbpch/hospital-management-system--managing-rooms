f2f23d254c5e3241ef865f6742236d2088277b27
rscbpch <bopich568317@gmail.com>
2025-11-01 00:12:54 +0700

    initialize dart project

---
+++ test/test.dart
@@ -0,0 +1,8 @@
+import 'package:hospital_management_system__managing_rooms/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
